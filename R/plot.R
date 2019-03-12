a77666b9c01feba11d28adfb7870eddb2679302e
Steven Chiou <schiou@utdallas.edu>
2019-03-12 14:10:27 -0500

    fixed bug in example

--- R/plot.R
+++ R/plot.R
@@ -121,7 +121,7 @@ plot.reSurv <- function(x, CSM = FALSE, order = TRUE, control = list(), ...) {
 #' dat$x3 <- ifelse(dat$x2 < 0, "x2 < 0", "x2 > 0")
 #' plotEvents(reSurv(Time, id, event, status) ~ x1 + x3, data = dat)
 #' ## With multiple hypothetical event types
-#' plotEvents(reSurv(Time, id, event * sample(1:3, 203, TRUE), status) ~
+#' plotEvents(reSurv(Time, id, event * sample(1:3, nrow(dat), TRUE), status) ~
 #'   x1, data = dat)
 plotEvents <- function(formula, data, order = TRUE, control = list(), ...) {
     ctrl <- plotEvents.control()
