8ab2fffbfc5b2349b872326f3accb9f3fc637bee
Steven Chiou <syhan.chiou@gmail.com>
2019-10-14 22:56:52 -0500

    added Recur from reda

--- R/Recur.R
+++ R/Recur.R
@@ -1,2 +1,34 @@
+#' \code{Recur} function imported from \code{reda}
+#'
+#' This is a placeholder.
+#' 
 #' @importFrom reda Recur
+#' @name export_Recur
+#' @noRd
+#' @aliases Recur
 #' @export Recur
+NULL
+
+
+#' \code{%2%} function imported from \code{reda}
+#'
+#' This is a placeholder.
+#' 
+#' @importFrom reda "%2%"
+#' @name export_Recur_to
+#' @noRd
+#' @aliases "%2%"
+#' @export %2%
+NULL
+
+#' \code{%to%} function imported from \code{reda}
+#'
+#' This is a placeholder.
+#' 
+#' @importFrom reda "%to%"
+#' @name export_Recur_to
+#' @noRd
+#' @aliases "%to%"
+#' @export %to%
+NULL
+
---
+++ inst/examples/ex_Recur.R
@@ -0,0 +1,11 @@
+library(reReg)
+
+args(Recur)
+?Recur
+Recur(3:5)
+Recur(6:1, id = rep(1:2, 3))
+
+time1 <- c(1, 5, 7)
+time2 <- c(3, 7, 9)
+ex3 <- Recur(time1 %to% time2, id = c("A1", "A1", "A2"))
+head(ex3)
