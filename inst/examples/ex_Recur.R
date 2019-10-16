5ef27507a89df1f776d301d8a9d48bc8284c3f06
Steven Chiou <syhan.chiou@gmail.com>
2019-10-15 23:45:17 -0500

    testing

--- inst/examples/ex_Recur.R
+++ inst/examples/ex_Recur.R
@@ -9,3 +9,23 @@ time1 <- c(1, 5, 7)
 time2 <- c(3, 7, 9)
 ex3 <- Recur(time1 %to% time2, id = c("A1", "A1", "A2"))
 head(ex3)
+
+## Testing readmission data
+data(readmission, package = "frailtypack")
+head(readmission)
+attach(readmission)
+
+Recur(t.start %2% t.stop, id, event, death)
+Recur(t.stop, id, event, death)
+
+reSurv(t.start, t.stop, id, event, death)
+reSurv(t.stop, id, event, death)
+
+detach(readmission)
+
+
+dat <- simSC(200, c(-1, 1), c(-1, 1))
+head(dat)
+
+with(dat, reSurv(Time, id, event, status))
+with(dat, Recur(Time, id, event, status))
