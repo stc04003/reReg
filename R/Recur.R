ba78c4b9a4bd4eb67100fd71e1d299a804aa7361
Steven Chiou <syhan.chiou@gmail.com>
2019-10-23 15:16:59 -0500

    fixed errors from roxygen

--- R/Recur.R
+++ R/Recur.R
@@ -1,44 +1,44 @@
-#' \code{Recur} function imported from \code{reda}
+#' The \code{Recur} function is imported from \code{reda}.
 #'
 #' This is a placeholder.
 #' 
 #' @importFrom reda Recur
-#' @name export_Recur
 #' @noRd
 #' @aliases Recur
 #' @export Recur
 NULL
 
-#' \code{is.Recur} function imported from \code{reda}
+#' The \code{is.Recur} function is imported from \code{reda}.
 #'
 #' This is a placeholder.
 #' 
 #' @importFrom reda is.Recur
-#' @name export_is.Recur
 #' @noRd
 #' @aliases is.Recur
 #' @export is.Recur
 NULL
 
-#' \code{%2%} function imported from \code{reda}
-#'
-#' This is a placeholder.
-#' 
-#' @importFrom reda "%2%"
-#' @name export_Recur_to
-#' @noRd
-#' @aliases "%2%"
-#' @export %2%
-NULL
+##' Recurrent Episodes
+##'
+##' Specify time segements or recurrent episodes by endpoints.
+##'
+##' This function is intended to be used for specifying the argument \code{time}
+##' in function \code{\link{Recur}}.
+##'
+##' @name Recur-to
+##'
+##' @param time1 The left end-points of the recurrent episodes.
+##' @param time2 The right end-points of the recurrent episodes.
+##'
+##' @return A list that consists of two elements named
+##'     \code{"time1"} and \code{"time2"}.
+##'
+##' @export
+`%to%` <- function(time1, time2) {
+    list(time1 = time1, time2 = time2)
+}
 
-#' \code{%to%} function imported from \code{reda}
-#'
-#' This is a placeholder.
-#' 
-#' @importFrom reda "%to%"
-#' @name export_Recur_to
-#' @noRd
-#' @aliases "%to%"
-#' @export %to%
-NULL
 
+##' @rdname Recur-to
+##' @export
+`%2%` <- `%to%`
