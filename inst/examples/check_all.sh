f90c5120e0100042c843397d4d99655a72c942ca
Steven Chiou <syhan.chiou@gmail.com>
2019-11-12 21:30:37 -0600

    added a bash file to loop through all example for individual valgrind check

---
+++ inst/examples/check_all.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+rm .RData
+rm .Rhistory
+rm *.Rout
+rm *.pdf
+
+for f in ex_*
+do 
+    echo "library(reReg);source('$f')" > "$f"2.R
+    R -d "valgrind --tool=memcheck --leak-check=full --log-file=$f"out --vanilla < "$f"2.R
+    rm "$f"2.R
+done
