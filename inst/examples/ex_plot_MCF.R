e2acc2e11d7e06be1de89a830e4192c802202c04
Steven Chiou <syhan.chiou@gmail.com>
2020-07-18 22:16:22 -0500

    cleaned examples

--- inst/examples/ex_plot_MCF.R
+++ inst/examples/ex_plot_MCF.R
@@ -1,4 +1,3 @@
 data(simDat)
 plotMCF(Recur(Time, id, event, status) ~ 1, data = simDat)
-plotMCF(Recur(Time, id, event, status) ~ x1, data = simDat)
 plotMCF(Recur(Time, id, event, status) ~ x1, data = simDat, onePanel = TRUE)
