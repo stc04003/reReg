a98d1b574bd07efd01df3fd6bbfed676c95f56c4
Wenjie Wang <wang@wwenjie.org>
2022-04-06 00:17:17 -0400

    Refer Recur to reda::Recur by ?Recur

--- R/Recur.R
+++ R/Recur.R
@@ -1,55 +1,19 @@
-#' The \code{Recur} function is imported from \code{reda}.
-#'
-#' Create a recurrent event survival object, used as a response variable in \code{reReg}.
-#' This function is replacing the original \code{reSurv()} in version 1.1.6.
-#' See \code{?reda::Recur} for more details.
-#'
-#' @rdname Recur
-#' @name Recur
-#' @aliases is.Recur
-#' @importFrom reda Recur is.Recur check_Recur
-#' @seealso \code{\link{\%2\%}}
-#' @importMethodsFrom reda show summary
-#' @exportMethod show summary
-#' @noRd
-#' @export Recur is.Recur
-#' @example inst/examples/ex_Recur.R
-NULL
+#' @importFrom reda Recur
+#' @export
+reda::Recur
 
-#' The \code{Recur} class is imported from \code{reda}.
-#'
-#' The class \code{Recur} is an S4 that represents a formula response for
-#' recurrent event data model. See \code{reda} for details.
-#'
-#' @noRd
-#' @rdname Recur-class
-#' @name Recur-class
-#' @importClassesFrom reda Recur 
-#' @exportClass Recur
-NULL
+#' @importFrom reda is.Recur
+#' @export
+reda::is.Recur
 
-#' The \code{summary.Recur} class is imported from \code{reda}.
-#'
-#' The class \code{summary.Recur} is an S4 that represents the summary of a \code{Recur} object.
-#' See \code{reda} for details.
-#'
-#' @noRd
-#' @rdname summary.Recur-class
-#' @name summary.Recur-class
-#' @importClassesFrom reda summary.Recur 
-#' @exportClass summary.Recur 
-NULL
+#' @importFrom reda check_Recur
+#' @export
+reda::check_Recur
 
-#' The \code{\%to\%} function is imported from \code{reda}
-#'
-#' This pipe operator specifies the time segments or recurrent episodes by endpoints.
-#' See \code{reda} for more details.
-#'
-#' @noRd
-#' @name Recur-pipe
-#' @rdname Recur-pipe
-#' @aliases %to% %2%
-#' @importFrom reda %to% %2%
-#' @export %to% %2%
-#' @example inst/examples/ex_Recur.R
-NULL
+#' @importFrom reda %to%
+#' @export
+reda::`%to%`
+
+#' @importFrom reda %2%
+#' @export
+reda::`%2%`
