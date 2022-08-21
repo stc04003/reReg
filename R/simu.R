4bcf0b401d974f79ae274551a3ebc809d23c3f94
Steven Chiou <syhan.chiou@gmail.com>
2022-08-21 11:49:05 -0500

    removed warnings

--- R/simu.R
+++ R/simu.R
@@ -2,20 +2,21 @@
 ## Codes to generate simulated data
 ###################################################################################
 
-#' Function to find inverse of a given Lam
+#' Function to find inverse of a given Lam or Haz
+#'
+#' Lam and Haz are cumulative rate and hazard functions, respectively.
 #' @noRd
-inv <- function (t, z, exa, exb, fun) {
-    mapply(t, FUN = function(u) {
-        uf <- function(x) u - fun(x, z, exa, exb) ## / Lam.f(10, r, b, model)
-        r1 <- dfsane(par = 1, function(y) uf(y), quiet = TRUE)
-        if (!r1$convergence) {
-            r2 <- exp(spg(par = 1, fn = function(y) uf(exp(y))^2, quiet = TRUE)$par)
-            if (uf(r1$par) <= uf(r2)) return(r1$par)
-            if (uf(r1$par) > uf(r2)) return(r2)
-        } else return(r1$par)
-        ## exp(optim(par = 1, fn = function(y) uf(exp(y))^2, 
-        ##           control = list(warn.1d.NelderMead = FALSE))$par)
-    })
+inv <- function (t, z, exa, exb, fun, tau) {
+  mapply(t, FUN = function(u) {
+    uf <- function(x) u - fun(x, z, exa, exb)
+    if (uf(0) * uf(tau) > 0) return(tau)
+    r1 <- dfsane(par = 1, function(y) uf(y), quiet = TRUE)
+    if (!r1$convergence) {
+      r2 <- exp(spg(par = 1, fn = function(y) uf(exp(y))^2, quiet = TRUE)$par)
+      if (uf(r1$par) <= uf(r2)) return(r1$par)
+      if (uf(r1$par) > uf(r2)) return(r2)
+    } else return(r1$par)
+  })
 }
 
 #' Function to generate simulated recurrent event data
@@ -91,113 +92,113 @@ inv <- function (t, z, exa, exb, fun) {
 #'
 #' @example inst/examples/ex_simu.R
 simGSC <- function(n, summary = FALSE, para,
-                  xmat, censoring, frailty, tau, origin,
-                  Lam0, Haz0) {
-    call <- match.call()
-    if (missing(tau)) tau <- 60
-    if (missing(origin)) origin <- 0
-    if (missing(frailty)) Z <- rgamma(n, 4, 4)
-    else Z <- frailty
-    if (missing(xmat)) {
-        X <- cbind(sample(0:1, n, TRUE), rnorm(n, sd = .5))
-        Cen <- runif(n, 0, X[,1] * tau * 2 + (1 - X[,1]) * 2 * Z^2 * tau)
+                   xmat, censoring, frailty, tau, origin,
+                   Lam0, Haz0) {
+  call <- match.call()
+  if (missing(tau)) tau <- 60
+  if (missing(origin)) origin <- 0
+  if (missing(frailty)) Z <- rgamma(n, 4, 4)
+  else Z <- frailty
+  if (missing(xmat)) {
+    X <- cbind(sample(0:1, n, TRUE), rnorm(n, sd = .5))
+    Cen <- runif(n, 0, X[,1] * tau * 2 + (1 - X[,1]) * 2 * Z^2 * tau)
+  } else {
+    if (!missing(censoring)) Cen <- censoring
+    if (missing(censoring)) Cen <- runif(n, 0, 2 * tau)
+    X <- xmat
+  }
+  
+  p <- ncol(X)
+  para0 <- list(alpha = rep(0, p), beta = rep(-1, p), eta = rep(0, p), theta = rep(1, p))
+  if (missing(para)) para <- para0
+  namel <- names(para)
+  para0[namel] <- para
+  alpha <- para0$alpha
+  beta <- para0$beta
+  eta <- para0$eta
+  theta <- para0$theta
+  msg.mismatch <- function(x)
+    paste("Parameter", substitute(x), "does not match with the number of covariates.")
+  if (length(alpha) != p) stop(msg.mismatch(alpha))
+  if (length(eta) != p) stop(msg.mismatch(eta))
+  if (length(beta) != p) stop(msg.mismatch(beta))
+  if (length(theta) != p) stop(msg.mismatch(theta))
+  ## lapply(list(alpha, eta, beta, theta), mismatch, p = p)
+  if (missing(Lam0)) {
+    Lam <- function(t, z, exa, exb) z * exb * log(1 + t * exa) / exa / .5
+    invLam <- function(t, z, exa, exb) (exp(.5 * t * exa / exb / z) - 1) / exa
+  } else {
+    Lam <- function(t, z, exa, exb) z * Lam0(t * exa) * exb / exa
+    invLam <- function(t, z, exa, exb) inv(t, z, exa, exb, Lam, tau)
+  }
+  if (missing(Haz0)) {
+    invHaz <- function(t, z, exa, exb) (exp(5 * t * exa / exb / z) - 1) / exa
+  } else {
+    Haz <- function(t, z, exa, exb) z * Haz0(t * exa) * exb / exa
+    invHaz <- function(t, z, exa, exb) inv(t, z, exa, exb, Haz, tau)
+  }
+  if (n != length(origin) & length(origin) > 1)
+    stop("Invalid length for 'origin'. See '?simGSC' for details.")
+  simOne <- function(id, z, x, cen) {
+    D <- invHaz(rexp(1), z, c(exp(x %*% eta)), c(exp(x %*% theta)))
+    y <- min(cen, tau, D)
+    status <- 1 * (y == D)
+    tij <- NULL
+    up <- Lam(y, z, c(exp(x %*% alpha)), c(exp(x %*% beta)))
+    m <- -1
+    while(sum(tij) < up) {
+      tij <- c(tij, rexp(1))
+      m <- m + 1
+    }
+    if (m > 0) {
+      tij <- invLam(cumsum(tij[1:m]), z, c(exp(x %*% alpha)), c(exp(x %*% beta)))
+      return(data.frame(id = id, Time = c(sort(tij), y),
+                        event = c(rep(1, m), 0), status = c(rep(0, m), status),
+                        Z = z, m = m, x = t(x)))
     } else {
-        if (!missing(censoring)) Cen <- censoring
-        if (missing(censoring)) Cen <- runif(n, 0, 2 * tau)
-        X <- xmat
+      return(data.frame(id = id, Time = y, event = 0, status = status,
+                        Z = z, m = m, x = t(x)))
     }
-    
-    p <- ncol(X)
-    para0 <- list(alpha = rep(0, p), beta = rep(-1, p), eta = rep(0, p), theta = rep(1, p))
-    if (missing(para)) para <- para0
-    namel <- names(para)
-    para0[namel] <- para
-    alpha <- para0$alpha
-    beta <- para0$beta
-    eta <- para0$eta
-    theta <- para0$theta
-    msg.mismatch <- function(x)
-        paste("Parameter", substitute(x), "does not match with the number of covariates.")
-    if (length(alpha) != p) stop(msg.mismatch(alpha))
-    if (length(eta) != p) stop(msg.mismatch(eta))
-    if (length(beta) != p) stop(msg.mismatch(beta))
-    if (length(theta) != p) stop(msg.mismatch(theta))
-    ## lapply(list(alpha, eta, beta, theta), mismatch, p = p)
-    if (missing(Lam0)) {
-        Lam <- function(t, z, exa, exb) z * exb * log(1 + t * exa) / exa / .5
-        invLam <- function(t, z, exa, exb) (exp(.5 * t * exa / exb / z) - 1) / exa
-    } else {
-        Lam <- function(t, z, exa, exb) z * Lam0(t * exa) * exb / exa
-        invLam <- function(t, z, exa, exb) inv(t, z, exa, exb, Lam)
-    }
-    if (missing(Haz0)) {
-        invHaz <- function(t, z, exa, exb) (exp(5 * t * exa / exb / z) - 1) / exa
-    } else {
-        Haz <- function(t, z, exa, exb) z * Haz0(t * exa) * exb / exa
-        invHaz <- function(t, z, exa, exb) inv(t, z, exa, exb, Haz)
-    }
-    if (n != length(origin) & length(origin) > 1)
-        stop("Invalid length for 'origin'. See '?simGSC' for details.")
-    simOne <- function(id, z, x, cen) {
-        D <- invHaz(rexp(1), z, c(exp(x %*% eta)), c(exp(x %*% theta)))
-        y <- min(cen, tau, D)
-        status <- 1 * (y == D)
-        tij <- NULL
-        up <- Lam(y, z, c(exp(x %*% alpha)), c(exp(x %*% beta)))
-        m <- -1
-        while(sum(tij) < up) {
-            tij <- c(tij, rexp(1))
-            m <- m + 1
-        }
-        if (m > 0) {
-            tij <- invLam(cumsum(tij[1:m]), z, c(exp(x %*% alpha)), c(exp(x %*% beta)))
-            return(data.frame(id = id, Time = c(sort(tij), y),
-                              event = c(rep(1, m), 0), status = c(rep(0, m), status),
-                              Z = z, m = m, x = t(x)))
-        } else {
-            return(data.frame(id = id, Time = y, event = 0, status = status,
-                              Z = z, m = m, x = t(x)))
-        }
-    }
-    dat <- data.frame(do.call(rbind, lapply(1:n, function(i) simOne(i, Z[i], X[i,], Cen[i]))))
-    names(dat)[grep("x.", names(dat))] <- paste0("x", 1:p)
-    if (length(origin) > 1) origin <- rep(origin, unlist(lapply(split(dat$id, dat$id), length)))
-    dat$t.start <- do.call(c, lapply(split(dat$Time, dat$id), function(x)
-        c(0, x[-length(x)]))) + origin
-    dat$t.stop <- dat$Time + origin
-    dat$Time <- NULL
-    if (summary) {
-        dg <- min(3, getOption("digits"))
-        cat("Call: \n")
-        print(call)
-        cat("\n")
-        cat("Summary:\n")
-        cat("Sample size:                                   ", n, "\n")
-        cat("Number of recurrent event observed:            ", sum(dat$event), "\n")
-        cat("Average number of recurrent event per subject: ", round(sum(dat$event) / n, dg), "\n")
-        cat("Proportion of subjects with a terminal event:  ", round(sum(dat$status) / n, dg), "\n")
-        base <- dat[cumsum(table(dat$id)), ]
-        y <- base$t.stop
-        d <- base$status
-        oy <- order(y)
-        d <- d[oy]
-        y <- y[oy]
-        r <- n - rank(y, ties.method = "min") + 1
-        is_first_y <- ! duplicated(y)
-        s <- cumprod(1 - (d / r)[is_first_y])
-        medTem <- ifelse(s[length(s)] > .5, NA, as.numeric(y[is_first_y][which.max(s - .5 < 0)]))
-        if (!is.na(medTem))
-            cat("Median time-to-terminal event:                 ",
-                round(medTem, dg), "\n")            
-        ## cat("Proportion of subjects with a x1 = 1:  ", round(mean(base$x1), dg), "\n")
-        cat("\n\n")
-    }
-    dat$m <- dat$Z <- NULL
-    ## dat <- dat[,c(1, 6:7, 2:5)]
-    ## Reorder columns
-    ord <- c("id", "t.start", "t.stop", "event", "status")
-    dat <- dat[,c(ord, setdiff(names(dat), ord))]
-    attr(dat, "Call") <- call
-    return(dat)
+  }
+  dat <- data.frame(do.call(rbind, lapply(1:n, function(i) simOne(i, Z[i], X[i,], Cen[i]))))
+  names(dat)[grep("x.", names(dat))] <- paste0("x", 1:p)
+  if (length(origin) > 1) origin <- rep(origin, unlist(lapply(split(dat$id, dat$id), length)))
+  dat$t.start <- do.call(c, lapply(split(dat$Time, dat$id), function(x)
+    c(0, x[-length(x)]))) + origin
+  dat$t.stop <- dat$Time + origin
+  dat$Time <- NULL
+  if (summary) {
+    dg <- min(3, getOption("digits"))
+    cat("Call: \n")
+    print(call)
+    cat("\n")
+    cat("Summary:\n")
+    cat("Sample size:                                   ", n, "\n")
+    cat("Number of recurrent event observed:            ", sum(dat$event), "\n")
+    cat("Average number of recurrent event per subject: ", round(sum(dat$event) / n, dg), "\n")
+    cat("Proportion of subjects with a terminal event:  ", round(sum(dat$status) / n, dg), "\n")
+    base <- dat[cumsum(table(dat$id)), ]
+    y <- base$t.stop
+    d <- base$status
+    oy <- order(y)
+    d <- d[oy]
+    y <- y[oy]
+    r <- n - rank(y, ties.method = "min") + 1
+    is_first_y <- ! duplicated(y)
+    s <- cumprod(1 - (d / r)[is_first_y])
+    medTem <- ifelse(s[length(s)] > .5, NA, as.numeric(y[is_first_y][which.max(s - .5 < 0)]))
+    if (!is.na(medTem))
+      cat("Median time-to-terminal event:                 ",
+          round(medTem, dg), "\n")            
+    ## cat("Proportion of subjects with a x1 = 1:  ", round(mean(base$x1), dg), "\n")
+    cat("\n\n")
+  }
+  dat$m <- dat$Z <- NULL
+  ## dat <- dat[,c(1, 6:7, 2:5)]
+  ## Reorder columns
+  ord <- c("id", "t.start", "t.stop", "event", "status")
+  dat <- dat[,c(ord, setdiff(names(dat), ord))]
+  attr(dat, "Call") <- call
+  return(dat)
 }
-               
+
